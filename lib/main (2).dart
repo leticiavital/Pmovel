11cac156fea3ce964eafffb6c9b51f2a161d81fb
mklara912 <mkvss1@aluno.ifal.edu.br>
2025-06-16 11:34:10 -0300

    Add files via upload

---
+++ lib/main (2).dart
@@ -0,0 +1,14 @@
+
+
+import 'package:infoclin/DicasdeexamesInfoclin.dart ';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      const MaterialApp(
+        home: DicasdeexamesInfoclin(),
+      )
+  );
+}
+
+
