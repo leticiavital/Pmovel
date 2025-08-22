cf125a8c71c0f07c3ea0512737ec2dbc5893ef70
ary0410 <mags12@aluno.ifal.edu.br>
2025-08-22 17:09:26 -0300

    medicos adicionados!!

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -42,6 +42,10 @@ class DBHelper {
     sql =
     "INSERT INTO Medico(nome, especialidade, estado, email, urlImage) VALUES ('Dra Luciana' ,'Cardiologia', 'PE', 'draluciana@gmail.com', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbi5rgOsTgdefvGMAfB12cx_fsegh04n_QWg&s');";
     await db.execute(sql);
+
+    sql =
+    "INSERT INTO Medico(nome, especialidade, estado, email, urlImage) VALUES ('Dr Roberto' ,'Cardiologia', 'PE', 'dr2roberto@gmail.com', '');";
+    await db.execute(sql);
   }
 
 }
